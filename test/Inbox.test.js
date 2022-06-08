10fa32914368e4d2827f1c95121a34332e884142
Bateristico <camilo.saa@gmail.com>
2022-06-08 20:48:24 +0200

    async and test deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,16 +3,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // instance of web3
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile')
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async() => {
     // get a list of all accounts
-    web3.eth.getAccounts().then(console.log);
-
+    accounts =  await web3.eth.getAccounts();
     // use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'})
+
+
 })
 
 describe('Inbox', ()=> {
     it('deploys a contract', ()=> {
-        // assert.ok(Inbox.address)
+        console.log(inbox)
     })
 })
