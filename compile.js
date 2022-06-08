f81169ccbbc9ae48b0b7e9f75277ba5af7417bc2
Bateristico <camilo.saa@gmail.com>
2022-06-08 17:53:47 +0200

    add compile code to smart contract

--- compile.js
+++ compile.js
@@ -1,1 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc')
+
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const inboxSource = fs.readFileSync(inboxPath, 'utf8');
+
+
 // compile code will go here
+module.exports = solc.compile(inboxSource, 1).contracts[':Inbox'];
+
